41b7343c78a2685cc961cdc4e7e6bb1d9c9e46b3
Abdelrahman Abouzaid <a.abouzaid404@gmail.com>
2025-08-05 18:27:39 +0300

    Add Database Initializer

--- SurveyBasket.Api/Program.cs
+++ SurveyBasket.Api/Program.cs
@@ -12,19 +12,7 @@ namespace SurveyBasket.Api
           
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.MapOpenApi();
-                app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            app.UseMiddlewares();
 
             app.Run();
         }
