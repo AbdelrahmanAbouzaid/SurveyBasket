3665d1edc3d8eeef396ee205fe78894d6aebff87
Abdelrahman Abouzaid <a.abouzaid404@gmail.com>
2025-08-24 21:06:04 +0300

    Add Cache Module

---
+++ SurveyBasket.Api/Sevices/CacheService.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace SurveyBasket.Api.Sevices
+{
+    public class CacheService(IDistributedCache distributedCache) : ICacheService
+    {
+        private readonly IDistributedCache _distributedCache = distributedCache;
+
+        public async Task<T?> GetAsync<T>(string key) where T : class
+        {
+            var value = await _distributedCache.GetStringAsync(key);
+           return string.IsNullOrEmpty(value) ? null :
+             JsonSerializer.Deserialize<T>(value);
+        }
+        public async Task SetAsync(string key, object value, TimeSpan? expiration = null)
+        {
+            await _distributedCache.SetStringAsync(key, JsonSerializer.Serialize(value), 
+                new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = expiration ?? TimeSpan.FromMinutes(60)
+                });
+        }
+        public async Task RemoveAsync(string key)
+        {
+            await _distributedCache.RemoveAsync(key);
+        }
+
+      
+    }
+}
